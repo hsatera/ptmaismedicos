3f74812ca56489970d2e2694eb031ff310c7bfbc
hsatera <65231064+hsatera@users.noreply.github.com>
2025-06-25 19:09:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -37,12 +37,6 @@ if uploaded_file is not None:
 
             df = df[required_columns]
 
-            # Normaliza a capitalização das colunas de texto para Title Case
-            # Primeiro converte para minúsculas, depois aplica o Title Case
-            for col in ['Município', 'Supervisor', 'Tutor', 'Nome Região']:
-                if col in df.columns:
-                    df[col] = df[col].astype(str).apply(lambda x: x.lower().title())
-
             # Limpa dados vazios nas colunas 'Supervisor' e 'Tutor'
             df['Supervisor'] = df['Supervisor'].fillna('')
             df['Tutor'] = df['Tutor'].fillna('')
